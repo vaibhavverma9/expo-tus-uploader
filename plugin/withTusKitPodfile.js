38c6795ee4885f70e4d4f03aae05a9cbee01f6b7
vaibhavverma9 <vaibhavverma9@uchicago.edu>
2025-06-02 23:14:56 -0400

    better targeting

--- plugin/withTusKitPodfile.js
+++ plugin/withTusKitPodfile.js
@@ -9,6 +9,7 @@ module.exports = function withTusKitPodfile(config) {
       const podfilePath = path.join(config.modRequest.platformProjectRoot, 'Podfile');
       let contents = fs.readFileSync(podfilePath, 'utf-8');
 
+      // Ensure use_frameworks! :linkage => :static is added inside the target block
       if (!contents.includes('use_frameworks! :linkage => :static')) {
         contents = contents.replace(
           /use_expo_modules!\n/,
@@ -16,10 +17,11 @@ module.exports = function withTusKitPodfile(config) {
         );
       }
 
-      if (!contents.includes("config.build_settings['SWIFT_VERSION'] = '5.0'")) {
+      // Add Swift version override inside post_install block
+      if (!contents.includes("config.build_settings['SWIFT_VERSION'] = '5.4'")) {
         contents = contents.replace(
           /post_install do \|installer\|([\s\S]*?)react_native_post_install\(([\s\S]*?)\)\n/,
-          `post_install do |installer|\n  react_native_post_install(\n    \\2\n  )\n\n  installer.pods_project.targets.each do |target|\n    target.build_configurations.each do |config|\n      config.build_settings['SWIFT_VERSION'] = '5.0'\n    end\n  end\n`
+          `post_install do |installer|\n  react_native_post_install(\n    \\2\n  )\n\n  installer.pods_project.targets.each do |target|\n    target.build_configurations.each do |config|\n      config.build_settings['SWIFT_VERSION'] = '5.4'\n    end\n  end\n`
         );
       }
 
